fe8230a2b36ad7a158683b49d7324b384cdd9145
franciscorafaelpires <franciscorafaelpires@gmail.com>
2025-04-10 15:57:07 -0300

    add calculadora como submodulo

---
+++ testeCalculadora/src/testeCalculadora.ts
@@ -0,0 +1,12 @@
+// testeCalculadora/src/testaCalculadora.ts
+import { Somador, Subtrator, Divisor, Multiplicador } from 'calculadora';
+
+const somador = new Somador();
+const subtrator = new Subtrator();
+const divisor = new Divisor();
+const multiplicador = new Multiplicador();
+
+console.log("Soma: ", somador.calcular(10, 5));         // 15
+console.log("Subtração: ", subtrator.calcular(10, 5));  // 5
+console.log("Divisão: ", divisor.calcular(10, 2));      // 5
+console.log("Multiplicação: ", multiplicador.calcular(10, 3)); // 30
